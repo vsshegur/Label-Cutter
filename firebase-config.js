399235ad37e492e4a2e065a39b9c2cf323bb4392
Vipul Shegur <vsshegur@gmail.com>
2026-08-24 14:58:30 +0530

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// firebase-config.js
+export const firebaseConfig = {
+  apiKey: "AIzaSyDSksHc8RucBx0qW_A6OBP6Ccn1g3hGKKE",
+  authDomain: "labelcutter-f7eb6.firebaseapp.com",
+  projectId: "labelcutter-f7eb6",
+  storageBucket: "labelcutter-f7eb6.firebasestorage.app",
+  messagingSenderId: "66842510048",
+  appId: "1:66842510048:web:02697d801892172e9d0685",
+  measurementId: "G-M42PECLE2Y"
+};
